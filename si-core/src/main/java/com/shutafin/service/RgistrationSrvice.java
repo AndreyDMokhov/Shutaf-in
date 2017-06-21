885dded72ffb0279e9069f22980237faa39955f0
Edward <edward.kats@outlook.com>
2017-06-21 21:08:16 +0300

    Edward Kats 1. Registration ...

---
+++ si-core/src/main/java/com/shutafin/service/RgistrationSrvice.java
@@ -0,0 +1,11 @@
+package com.shutafin.service;
+
+/**
+ * Created by Edward on 6/21/2017.
+ */
+public class RgistrationSrvice {
+
+    public static void method() {
+        System.out.println("Some text");
+    }
+}
