a72fdb6e67aa1d3c71ec06807a4766d9de7cc984
khomenker <autohelp123@gmail.com>
2017-06-22 12:58:22 +0300

    Shimon Khomenker 1. in app.route.js added state "registration" 2. in header.html added tab "registration" 3. in index.html added src to registration controller 4. added changes to lang-en & lang-ru 5. created registration.html 6. in registration.html created inputs , validations 7. created registration.ctrl.js 8. in registration.ctrl.js added controller registration & directive for validation password 9. created registration.model.js 10. in registration.model.js created model registration

---
+++ si-webapp/src/client/src/partials/registration/registration.ctrl.js
@@ -0,0 +1,39 @@
+app.controller('userRegistration', function () {
+    var vm = this;
+
+    vm.registrationData = {}
+
+    function registerUser() {
+        console.log(vm.registrationData);
+        registrationModel.registerUser(vm.registrationData).then(
+    function (success) {
+        alert('Registration is ok');
+
+    },function (error) {
+                alert("Registration fail");
+                console.log(error);
+            }
+        )
+    }
+
+    vm.registerUser = registerUser;
+});
+
+app.directive("matchPassword", function () {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=matchPassword"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.matchPassword12 = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+});
