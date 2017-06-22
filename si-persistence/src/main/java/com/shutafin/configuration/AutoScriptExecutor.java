f06b059ba1ab4b1a5e767f76a14c56181571d507
xciter <esilevsky@gmail.com>
2017-06-22 18:24:03 +0300

    Ivan Yesilevskyi 1. Changed logic to execute SQL scripts as Resources instead of parsing SQL queries. 2. Add new beans to xml configuration. 3. Moved SQL script of creating DB_UPDATE table to static final values 4. Changed package name 5. Handled exceptions are rethrowed

---
+++ si-persistence/src/main/java/com/shutafin/configuration/AutoScriptExecutor.java
@@ -0,0 +1,122 @@
+package com.shutafin.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.jdbc.datasource.init.ScriptStatementFailedException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+//TODO: change to transactions in order to rollback in case of errors
+
+@Component
+public class AutoScriptExecutor implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final static String DB_UPDATE_TABLE_NAME = "db_update";
+    private final static String CREATE_DB_UPDATE_TABLE_SCRIPT = "CREATE TABLE `DB_UPDATE`" +
+            " (`SCRIPT_ID` INT);";
+    private final static String INSERT_BASIC_VAL_SCRIPT = "INSERT INTO `DB_UPDATE`(`SCRIPT_ID`) VALUES (0);";
+
+    private TreeMap<Integer, Resource> sqlScripts = new TreeMap<>();
+    private Integer lastExecutedSqlScript;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private DataSource dataSource;
+
+    public void checkDatabase() {
+
+        getLastExecutedScript();
+        getAllSqlScripts();
+
+        for (Map.Entry<Integer, Resource> script :
+                sqlScripts.tailMap(lastExecutedSqlScript, false).entrySet()) {
+            executeSqlScript(script.getValue());
+        }
+        updateLastExecutedScript(sqlScripts.lastKey());
+    }
+
+    private void getLastExecutedScript() {
+        String showTablesQuery = "SHOW TABLES;";
+        List<String> tables = jdbcTemplate.queryForList(showTablesQuery, String.class);
+        if (tables.contains(DB_UPDATE_TABLE_NAME)) {
+            String selectQuery = "SELECT script_id FROM DB_UPDATE;";
+            lastExecutedSqlScript = jdbcTemplate.queryForObject(selectQuery, Integer.class);
+        } else {
+            initDbUpdateTable();
+            lastExecutedSqlScript = 0;
+        }
+    }
+
+    private void initDbUpdateTable() {
+        executeQuery(CREATE_DB_UPDATE_TABLE_SCRIPT);
+        executeQuery(INSERT_BASIC_VAL_SCRIPT);
+    }
+
+    private void updateLastExecutedScript(int lastScriptNumber) {
+        String updateQuery = "UPDATE DB_UPDATE SET script_id=" + lastScriptNumber;
+        executeQuery(updateQuery);
+    }
+
+    private void getAllSqlScripts() {
+        try {
+            ClassLoader cl = this.getClass().getClassLoader();
+            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(cl);
+            Resource[] resources = resolver.getResources("classpath:sql/*.sql");
+            for (Resource res : resources) {
+                sqlScripts.put(getFileNumber(res), res);
+            }
+        } catch (IOException exp) {
+            System.out.println("Error while getting SQL scripts");
+        }
+    }
+
+    private Integer getFileNumber(Resource file) {
+        try {
+            Integer fileNumber = Integer.parseInt(file.getFilename().split("\\.")[0]);
+            return fileNumber;
+        } catch (NumberFormatException exp) {
+            System.out.println("failed to parse number");
+        }
+        return -1;
+    }
+
+    private void executeQuery(String query) {
+        try {
+            jdbcTemplate.execute(query);
+        } catch (Exception exp) {
+            System.out.println("Error while executing query\n" + exp);
+            throw exp;
+        }
+    }
+
+    @Transactional("dsTransactionManager")
+    private void executeSqlScript(Resource script) {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator(script);
+        try {
+            databasePopulator.execute(dataSource);
+            lastExecutedSqlScript = getFileNumber(script);
+        } catch (ScriptStatementFailedException exp) {
+            System.out.println("Error while executing SQL Script");
+            throw exp;
+        }
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        checkDatabase();
+    }
+}
