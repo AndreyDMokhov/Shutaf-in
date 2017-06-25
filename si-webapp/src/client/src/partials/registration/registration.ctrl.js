0059270cbc56b8ebb9ddf62bb15250547ca8dfb1
khomenker <autohelp123@gmail.com>
2017-06-25 15:46:02 +0300

    Shimon Khomenker 1. in lang-en fixed translation 2. in  registration.ctrl  deleted console.log and alerts, added notifiers.  Remake confirm password 3. registration.html remake confirm password 4 fixed all comments from pull requests

--- si-webapp/src/client/src/partials/registration/registration.ctrl.js
+++ si-webapp/src/client/src/partials/registration/registration.ctrl.js
@@ -1,39 +1,21 @@
-app.controller('userRegistration', function (registrationModel, $http) {
+app.controller('userRegistration', function (registrationModel, $http, notify, $state) {
     var vm = this;
-
     vm.registrationData = {};
 
     function registerUser() {
         console.log(vm.registrationData);
         registrationModel.registerUser(vm.registrationData).then(
-            function (success ) {
-                localStorage.setItem("session_Id", success.headers('session_id'))
-                console.log(success.headers('session_id'));
-                alert('Registration is ok');
-
-            },function (error) {
-                alert("Registration fail");
-                console.log(error);
+            function (success) {
+                localStorage.setItem("session_Id", success.headers('session_id'));
+                notify.set('Registration is ok', {type: 'success'});
+                $state.go("home");
+            }, function (error) {
+                notify.set("Registration fail", {type: 'error'});
             })
     };
+
     vm.registerUser = registerUser;
+
 });
 
-app.directive("matchPassword", function () {
-    return {
-        require: "ngModel",
-        scope: {
-            otherModelValue: "=matchPassword"
-        },
-        link: function(scope, element, attributes, ngModel) {
 
-            ngModel.$validators.matchPassword12 = function(modelValue) {
-                return modelValue == scope.otherModelValue;
-            };
-
-            scope.$watch("otherModelValue", function() {
-                ngModel.$validate();
-            });
-        }
-    };
-});
