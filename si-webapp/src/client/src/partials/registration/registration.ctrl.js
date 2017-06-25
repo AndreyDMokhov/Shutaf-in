85d6d3ea05be18962b95d87b0d179c75a3d975d0
Edward <edward.kats@outlook.com>
2017-06-26 01:41:30 +0300

    Registration 1. Image loader moved to a file instead of base64 2. Fixed image loader loading 3. Moved image loader withing the registration button

--- si-webapp/src/client/src/partials/registration/registration.ctrl.js
+++ si-webapp/src/client/src/partials/registration/registration.ctrl.js
@@ -2,15 +2,20 @@ app.controller('userRegistration', function (registrationModel, notify, $state, 
     var vm = this;
     vm.registrationData = {};
 
+    vm.dataLoading = false;
+
 
     function registerUser() {
         console.log(vm.registrationData);
+        vm.dataLoading = true;
         registrationModel.registerUser(vm.registrationData).then(
             function (success) {
+                vm.dataLoading = false;
                 localStorage.setItem("session_Id", success.headers('session_id'));
                 notify.set($filter('translate')("Registration.form.msg.registrationOK"), {type: 'success'});
                 $state.go("home");
             }, function (error) {
+                vm.dataLoading = false;
                 notify.set($filter('translate')("Registration.form.msg.registrationFail"), {type: 'error'});
             })
     };
