92845698ab74a956f5270910df4449b9371e87bd
Alexander Matsievsky <matsievsky@gmail.com>
2017-07-04 15:16:52 +0300

    AlexM Update: UserAccountSettingsServiceImpl update save & update get

--- si-core/src/main/java/com/shutafin/service/impl/UserAccountSettingsServiceImpl.java
+++ si-core/src/main/java/com/shutafin/service/impl/UserAccountSettingsServiceImpl.java
@@ -7,7 +7,9 @@ import com.shutafin.model.entities.UserSession;
 import com.shutafin.model.entities.infrastructure.Language;
 import com.shutafin.model.web.user.UserAccountSettingsWeb;
 import com.shutafin.repository.UserAccountRepository;
+import com.shutafin.repository.UserRepository;
 import com.shutafin.repository.UserSessionRepository;
+import com.shutafin.repository.infrastructure.LanguageRepository;
 import com.shutafin.service.UserAccountSettingsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,42 +29,65 @@ public class UserAccountSettingsServiceImpl implements UserAccountSettingsServic
     private UserSessionRepository userSessionRepository;
     @Autowired
     private UserAccountRepository userAccountRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private LanguageRepository languageRepository;
 
     @Override
     @Transactional
     public void save(UserAccountSettingsWeb userAccountSettingsWeb, String sessionId) {
-        String firstName = userAccountSettingsWeb.getFirstName();
-        String lastNameWeb = userAccountSettingsWeb.getLastName();
         UserSession userSession = userSessionRepository.findSessionBySessionId(sessionId);
         if (!isExpirationDateAndValid(userSession)) {
             throw new AuthenticationException();
         }
-
         User user = userSession.getUser();
-//        System.out.println(user);
-        if (user.getFirstName().equals(firstName)) {
-            user.setLastName(lastNameWeb);
-        }else if (user.getLastName().equals(lastNameWeb)) {
-            user.setFirstName(firstName);
-        }else{
-            user.setFirstName(firstName);
-            user.setLastName(lastNameWeb);
+        if (updateLanguage(user, userAccountSettingsWeb) || updateFirstLastNames(user, userAccountSettingsWeb)) {
+            userRepository.update(user);
         }
+    }
+
+    private boolean updateFirstLastNames(User user, UserAccountSettingsWeb userAccountSettingsWeb) {
+        String firstName = userAccountSettingsWeb.getFirstName();
+        String lastNameWeb = userAccountSettingsWeb.getLastName();
+        if(!user.getFirstName().equals(firstName) && !user.getLastName().equals(lastNameWeb)) {
+            if (user.getFirstName().equals(firstName)) {
+                user.setLastName(lastNameWeb);
+            }
+            if (user.getLastName().equals(lastNameWeb)) {
+                user.setFirstName(firstName);
+            }
+            user.setFirstName(firstName);
+            user.setLastName(lastNameWeb);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean updateLanguage(User user, UserAccountSettingsWeb userAccountSettingsWeb) {
+        String languageId = userAccountSettingsWeb.getLanguageId();
+        UserAccount userAccount = userAccountRepository.getAccountUserByUserId(user);
+        String languageDB = userAccount.getLanguage().getDescription();
+        if (!languageDB.equals(languageId)){
+            Language language = languageRepository.findById(languageId);
+            userAccount.setLanguage(language);
+            userAccountRepository.update(userAccount);
+            return true;
+        }
+        return false;
     }
 
     @Override
     @Transactional
     public UserAccountSettingsWeb get(String sessionId) {
         UserSession userSession = userSessionRepository.findSessionBySessionId(sessionId);
-
         if (!isExpirationDateAndValid(userSession)) {
             throw new AuthenticationException();
         }
         User user = userSession.getUser();
         UserAccount userAccount = userAccountRepository.getAccountUserByUserId(user);
         Language language = userAccount.getLanguage();
-
-        return new UserAccountSettingsWeb(user.getFirstName(), user.getLastName(), language.getLanguageNativeName());
+        return new UserAccountSettingsWeb(user.getFirstName(), user.getLastName(), language.getDescription());
     }
 
     private boolean isExpirationDateAndValid(UserSession userSession) {
