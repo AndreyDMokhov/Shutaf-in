e4a093320de26fe7953bab89dfc6a76d1364175f
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-07-13 17:31:45 +0300

    Anatoliy Tsybikov 1 EnvironmentConfigurationServiceImpl -> update @PropertySource @Value

--- si-core/src/main/java/com/shutafin/service/impl/EnvironmentConfigurationServiceImpl.java
+++ si-core/src/main/java/com/shutafin/service/impl/EnvironmentConfigurationServiceImpl.java
@@ -1,45 +1,30 @@
 package com.shutafin.service.impl;
 
 import com.shutafin.service.EnvironmentConfigurationService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Properties;
 
 @Service
+@PropertySource("application.properties")
 public class EnvironmentConfigurationServiceImpl implements EnvironmentConfigurationService {
 
-    private static final String APPLICATION_PROPERTIES = "si-core/src/main/resources/application.properties";
-    private static final String EXTERNAL_PORT = "external.port";
+    @Value("${external.port}")
+    private String externalPort;
 
     @Override
     public String getServerAddress() {
-        Properties prop = getProperties();
-        String externalPort = prop.getProperty(EXTERNAL_PORT);
-        InetAddress addr;
+        String ip;
         try {
-            addr = InetAddress.getLocalHost();
+            ip = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
             e.printStackTrace();
             throw new IllegalStateException("Unexpected error occurred");
         }
-        String ip = addr.getHostAddress();
         return ip + ":" + externalPort;
-    }
-
-    private Properties getProperties() {
-        Properties prop = new Properties();
-        FileInputStream input;
-        try {
-            input = new FileInputStream(APPLICATION_PROPERTIES);
-            prop.load(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new IllegalStateException("Unexpected error occurred");
-        }
-        return prop;
     }
 
 }
