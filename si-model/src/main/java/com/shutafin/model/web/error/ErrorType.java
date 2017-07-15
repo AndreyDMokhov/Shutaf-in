69108b544138ebb612b8a3c54f5d99e9cdf6862a
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-07-15 21:58:09 +0300

    Anatoliy Tsybikov Merge branch 'develop' into Anatoliy/reset-password

--- si-model/src/main/java/com/shutafin/model/web/error/ErrorType.java
+++ si-model/src/main/java/com/shutafin/model/web/error/ErrorType.java
@@ -3,21 +3,12 @@ package com.shutafin.model.web.error;
 
 public enum ErrorType {
 
-<<<<<<< HEAD
-    INPUT(400, "INP"),
-    SYSTEM(500, "SYS"),
-    AUTHENTICATION(401, "AUT"),
-    EMAIL_SEND_ERROR(503, "ESE"),
-    EMAIL_CONFIGURATION_ERROR(500, "ECE"),
-    RESOURCE_NOT_FOUND(404, "RNF");
-=======
     INPUT(400, ErrorCode.INP),
     SYSTEM(500, ErrorCode.SYS),
     AUTHENTICATION(401, ErrorCode.AUT),
     EMAIL_SEND_ERROR(503, ErrorCode.ESE),
     EMAIL_CONFIGURATION_ERROR(500, ErrorCode.ECE),
     EMAIL_DUPLICATION_EXCEPTION(400, ErrorCode.EDE);
->>>>>>> develop
 
     private Integer httpCode;
     private ErrorCode errorCodeType;
