bc94daa35a8733d2a6c4410e0ed99e98a124aa8c
Marina <maryna.pekh@gmail.com>
2017-07-17 11:38:40 +0300

    Marina Pekh changed functions names, value of request mapping and request method (from POST to PUT)

--- si-webapp/src/client/src/partials/securitySettingsPassword/securitySettingsPassword.model.js
+++ si-webapp/src/client/src/partials/securitySettingsPassword/securitySettingsPassword.model.js
@@ -1,16 +1,15 @@
 app.factory('securitySettingsPasswordModel', function (Restangular) {
 
     var rest = Restangular.withConfig(function (RestangularProvider) {
-        RestangularProvider.setBaseUrl('/api/account/security-settings/password');
+        RestangularProvider.setBaseUrl('/api/account/security-settings');
         RestangularProvider.setDefaultHeaders({'session_id':localStorage.getItem('session_id')});
     });
 
-    function updatePassword(param) {
-
+    function changePassword(param) {
         return  rest.one('/password').customPUT(param)
-    };
+    }
 
     return{
-        updatePassword: updatePassword
+        changePassword: changePassword
     }
 });
