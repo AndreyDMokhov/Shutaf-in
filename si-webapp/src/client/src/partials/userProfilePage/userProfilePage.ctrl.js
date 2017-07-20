c2294ddd26bd0a23d640ec1eb3f33271d0ceefb2
khomenker <autohelp123@gmail.com>
2017-07-21 02:36:53 +0300

    Shimon Khomenker

--- si-webapp/src/client/src/partials/userProfilePage/userProfilePage.ctrl.js
+++ si-webapp/src/client/src/partials/userProfilePage/userProfilePage.ctrl.js
@@ -3,19 +3,22 @@ app.controller('userProfilePage', function ($state, $filter, sessionService, $sc
     vm.userProfile = JSON.parse(sessionStorage.getItem('userProfile'));
     vm.infofiles={"filename":"AAA"};
     vm.avatarImage = '../../images/default_avatar.png';
-    // console.log(vm.infofiles)
     $scope.onChange = function (e, fileList) {
         alert('this is on-change handler!');
     };
 
     function onLoad (e, reader, file, fileList, fileOjects, fileObj) {
         console.log(vm.infofiles)
-    };
+        };
 
     function try1 () {
-        onLoad();
+        var number=vm.infofiles.base64
         console.log(vm.infofiles.filename)
+        console.log(number)
+        // console.log('"data:image/jpeg;base64,' + vm.number + '"')
 
+
+        vm.avatarImage='data:image/jpeg;base64,' + vm.infofiles.base64
     };
 
 
