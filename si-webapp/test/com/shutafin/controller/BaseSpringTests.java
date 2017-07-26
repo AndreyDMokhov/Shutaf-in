2e5593b20e8236d932402ea97197220594330df5
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-07-26 14:29:46 +0300

    Anatoliy Tsybikov 1 update BaseSpringTests

--- si-webapp/test/com/shutafin/controller/BaseSpringTests.java
+++ si-webapp/test/com/shutafin/controller/BaseSpringTests.java
@@ -3,10 +3,7 @@ package com.shutafin.controller;
 
 import com.google.gson.Gson;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
 
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +15,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -50,7 +42,11 @@ public class BaseSpringTests {
                 .andDo(print())
                 .andReturn();
 
-        ApiResponse apiResponse = gson.fromJson(result.getResponse().getContentAsString(), ApiResponse.class);
+        ApiResponse apiResponse = new ApiResponse();
+        String json = result.getResponse().getContentAsString();
+        if (!json.equals("")){
+            apiResponse = gson.fromJson(json, ApiResponse.class);
+        }
         apiResponse.setStatus(result.getResponse().getStatus());
 
         return apiResponse;
--- si-webapp/test/com/shutafin/controller/Test.java
+++ si-webapp/test/com/shutafin/controller/Test.java
@@ -5,15 +5,17 @@ public class Test extends BaseSpringTests{
     @org.junit.Test
     public void registration200Ok() throws Exception {
         String uri = "/users/registration/request";
-        String bodyJSON = "{\"firstName\":\"bb\",\"lastName\":\"bb\",\"email\":\"bbb\",\"password\":\"111111Zz\",\"userLanguageId\":\"1\"}";
+        String bodyJSON = "{\"firstName\":\"bb\",\"lastName\":\"bb\",\"email\":\"bbhbbb\",\"password\":\"111111Zz\",\"userLanguageId\":\"1\"}";
 
         ApiResponse apiResponse = test(uri, bodyJSON);
 
         System.out.println("------apiResponse---------");
         System.out.println(apiResponse.getStatus());
-        System.out.println(apiResponse.getError().getErrorMessage());
-        System.out.println(apiResponse.getError().getErrorTypeCode());
-        System.out.println(apiResponse.getError().getErrors());
+        if (apiResponse.getError() != null){
+            System.out.println(apiResponse.getError().getErrorMessage());
+            System.out.println(apiResponse.getError().getErrorTypeCode());
+            System.out.println(apiResponse.getError().getErrors());
+        }
         System.out.println(apiResponse.getData());
     }
 }
