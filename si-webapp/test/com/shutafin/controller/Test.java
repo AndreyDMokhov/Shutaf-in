d9d98b072dba56ee4b8c0418460dee473df3e014
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-07-26 12:55:40 +0300

    Anatoliy Tsybikov 1 RegistrationControllerTest -> update

---
+++ si-webapp/test/com/shutafin/controller/Test.java
@@ -0,0 +1,20 @@
+package com.shutafin.controller;
+
+public class Test extends BaseSpringTests{
+
+    @org.junit.Test
+    public void registration200Ok() throws Exception {
+        String uri = "/users/registration/request";
+        String bodyJSON = "{\"firstName\":\"bb\",\"lastName\":\"bb\",\"email\":\"bbb\",\"password\":\"111111Zz\",\"userLanguageId\":\"1\"}";
+
+        ApiResponse apiResponse = test(uri, bodyJSON);
+
+        System.out.println("------apiResponse---------");
+        System.out.println(apiResponse.getStatus());
+        System.out.println(apiResponse.getError().getErrorMessage());
+        System.out.println(apiResponse.getError().getErrorTypeCode());
+        System.out.println(apiResponse.getError().getErrors());
+        System.out.println(apiResponse.getData());
+    }
+
+}
