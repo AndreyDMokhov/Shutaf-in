3b4e3674bf36b6f71a922b2f65c31844547c66d1
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-07-26 14:36:45 +0300

    Anatoliy Tsybikov Merge branch 'develop' into Anatoliy/registrationControllerTest

--- si-persistence/src/main/java/com/shutafin/repository/UserAccountRepository.java
+++ si-persistence/src/main/java/com/shutafin/repository/UserAccountRepository.java
@@ -5,11 +5,9 @@ import com.shutafin.model.entities.UserAccount;
 import com.shutafin.model.entities.infrastructure.Language;
 import com.shutafin.repository.base.PersistentDao;
 
-/**
- * Created by evgeny on 6/20/2017.
- */
+
 public interface UserAccountRepository extends PersistentDao<UserAccount> {
-    public Language findUserLanguage(User user);
-    public void updateUserLanguage(Integer languageId, User user);
+    Language findUserLanguage(User user);
+    void updateUserLanguage(Integer languageId, User user);
     UserAccount findUserAccountByUser(User user);
 }
