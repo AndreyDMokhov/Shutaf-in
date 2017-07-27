e2c389628cc957eff1f8afc523e7dd1de971347e
Edward <edward.kats@outlook.com>
2017-07-27 03:59:45 +0300

    Edward Kats 1. Added all-cases-reusable test API for proceeding requests 2. Added Lombok

--- si-webapp/src/test/java/com/shutafin/controller/APIWebResponseDeserializer.java
+++ si-webapp/src/test/java/com/shutafin/controller/APIWebResponseDeserializer.java
@@ -4,25 +4,28 @@ import com.google.gson.*;
 import com.shutafin.model.web.APIWebResponse;
 import com.shutafin.model.web.DataResponse;
 import com.shutafin.model.web.error.ErrorResponse;
+import com.shutafin.model.web.error.ErrorType;
 import com.shutafin.model.web.error.errors.InputValidationError;
 
 import java.lang.reflect.Type;
 
 public class APIWebResponseDeserializer implements JsonDeserializer<APIWebResponse> {
+
     @Override
     public APIWebResponse deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        DataResponse data =jsonDeserializationContext.deserialize(jsonElement.getAsJsonObject().get("data"), DataResponse.class);
+        DataResponse data = jsonDeserializationContext.deserialize(jsonElement.getAsJsonObject().get("data"), DataResponse.class);
 
         JsonObject error = jsonElement.getAsJsonObject().get("error").getAsJsonObject();
         String errorTypeCode = error.getAsJsonObject().get("errorTypeCode").getAsString();
 
         ErrorResponse errorResponse;
-        if (errorTypeCode.equals("INP")){
+        ErrorType errorType = ErrorType.getById(errorTypeCode);
+
+        if (errorType == ErrorType.INPUT) {
             errorResponse = jsonDeserializationContext.deserialize(jsonElement.getAsJsonObject().get("error"), InputValidationError.class);
-        }else {
+        } else {
             errorResponse = jsonDeserializationContext.deserialize(jsonElement.getAsJsonObject().get("error"), ErrorResponse.class);
         }
-
 
 
         return new APIWebResponse(errorResponse, data);
