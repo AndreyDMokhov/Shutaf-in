52c2c3501de675fbd194b1ff7ee28c93854528f7
evgeny_cube <evgeny.yablokov@gmail.com>
2017-07-28 01:11:08 -0700

    Evgeny Yablokov 1. exclude commons-logging in root pom.xml 2. define bridge logging from JCL to SLF4j in pom.xml 3. added logback.xml to si-webapp/src/main/resources 4. added log messages to InitializationController

--- si-webapp/src/main/java/com/shutafin/controller/InitializationController.java
+++ si-webapp/src/main/java/com/shutafin/controller/InitializationController.java
@@ -1,6 +1,9 @@
 package com.shutafin.controller;
 
 import com.shutafin.service.InitializationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +22,17 @@ public class InitializationController {
     @Autowired
     private InitializationService initializationService;
 
+    private static final Logger logger = LoggerFactory.getLogger(InitializationController.class);
+
     @RequestMapping(value = "/all", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public Map<String, List> getAll(){
+
+        logger.info("The info message is '{}'.", new String("some INFO message"));
+        logger.trace("The trace message is '{}'.", new String("some TRACE message"));
+        logger.debug("The debug message is '{}'.", new String("some DEBUG message"));
+        logger.warn("The warn message is '{}'.", new String("some WARN message"));
+        logger.error("The exception is '{}'.", new Exception("some exception"));
+
         return new HashMap<String, List>() {{
             put("languages", initializationService.findAllLanguages());
         }};
