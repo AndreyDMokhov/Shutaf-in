ef0516f2690d84a4a31ece8ccbce542b4c4e9ebf
edward_ka <edward_ka@malam.com>
2017-07-31 18:49:19 +0300

    Edward Kats Matched test to fixed logout logic

--- si-webapp/src/test/java/com/shutafin/controller/LogoutControllerTest.java
+++ si-webapp/src/test/java/com/shutafin/controller/LogoutControllerTest.java
@@ -1,8 +1,10 @@
 package com.shutafin.controller;
 
+import com.shutafin.model.entities.UserSession;
 import com.shutafin.model.web.APIWebResponse;
 import com.shutafin.model.web.error.ErrorType;
 import com.shutafin.service.LogoutService;
+import com.shutafin.service.SessionManagementService;
 import com.shutafin.system.BaseTestImpl;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,12 +27,16 @@ public class LogoutControllerTest extends BaseTestImpl{
 
     public static final String LOGOUT_REQUEST_URL = "/logout/";
 
-    @Autowired
+    @MockBean
     private LogoutService logoutService;
+
+    @MockBean
+    private SessionManagementService sessionManagementService;
 
     @Before
     public void setUp(){
-        Mockito.doNothing().when(logoutService).logout(anyString());
+        Mockito.doNothing().when(logoutService).logout(any(UserSession.class));
+        Mockito.when(sessionManagementService.findValidUserSession(anyString())).thenReturn(new UserSession());
     }
 
     @Test
@@ -46,13 +53,6 @@ public class LogoutControllerTest extends BaseTestImpl{
         Assert.assertEquals(response.getError().getErrorTypeCode(), ErrorType.AUTHENTICATION.getErrorCodeType());
     }
 
-    @Test
-    public void LogoutRequest_IncorrectSessionIdHeader_Length35(){
-        List<HttpHeaders> headers = addSessionIdToHeader("4042cd8-51d0-4282-b431-36ee7f6dcaef");
-        APIWebResponse response = getResponse(LOGOUT_REQUEST_URL, HttpMethod.POST, headers);
-        Assert.assertNotNull(response.getError());
-        Assert.assertEquals(response.getError().getErrorTypeCode(), ErrorType.AUTHENTICATION.getErrorCodeType());
-    }
 
     public List<HttpHeaders> addSessionIdToHeader(String sessionId){
         List<HttpHeaders> headers = new ArrayList<>();
