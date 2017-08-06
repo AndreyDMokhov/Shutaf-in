1c71dd856326d71ad3e77973fed9ec48ad4d7bf3
xciter <esilevsky@gmail.com>
2017-08-06 21:20:54 +0300

    Ivan Yesilevskyi
    1. Changed UserImageService logic: getUserImage first try to get image from
    file system, in case of failure it returns image from DB.
    Side effect: created date returned could differ from actual in DB
    (due to using time of image modification), in order to avoid unreasonable query to DB.

--- si-core/src/main/java/com/shutafin/service/impl/UserImageServiceImpl.java
+++ si-core/src/main/java/com/shutafin/service/impl/UserImageServiceImpl.java
@@ -13,12 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.time.Instant;
 import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -59,7 +57,11 @@ public class UserImageServiceImpl implements UserImageService {
     @Override
     @Transactional
     public UserImage getUserImage(User user, Long userImageId) {
-        UserImage userImage = userImageRepository.findById(userImageId);
+        UserImage userImage = getUserImageFromFileSystem(user, userImageId);
+        if (userImage != null) {
+            return userImage;
+        }
+        userImage = userImageRepository.findById(userImageId);
         if (userImage == null || !user.getId().equals(userImage.getUser().getId())) {
             throw new ResourceNotFoundException(String.format("User Image with ID %d was not found", userImageId));
         }
@@ -118,6 +120,34 @@ public class UserImageServiceImpl implements UserImageService {
             e.printStackTrace();
             System.out.println("Error while saving image to file\n" + e);
         }
+    }
+
+    private UserImage getUserImageFromFileSystem(User user, Long userImageId) {
+        UserImage userImage = new UserImage();
+        userImage.setId(userImageId);
+        userImage.setUser(user);
+        userImage.setImageStorage(new ImageStorage());
+        String imageLocalPath = getUserDirectoryPath(user) + String.valueOf(userImageId) + IMAGE_EXTENSION;
+        userImage.setLocalPath(imageLocalPath);
+        File imageFile = new File(imageLocalPath);
+        try {
+            if (imageFile.exists()) {
+                FileInputStream inputStream = new FileInputStream(imageFile);
+                byte[] imageDecoded = new byte[inputStream.available()];
+                inputStream.read(imageDecoded);
+                inputStream.close();
+                userImage.setCreatedDate(Date.from(Instant.ofEpochMilli(imageFile.lastModified())));
+                userImage.getImageStorage().setImageEncoded(Base64.getEncoder().encodeToString(imageDecoded));
+                return userImage;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File not found\n" + e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error while reading image from file system\n" + e);
+        }
+        return null;
     }
 
     private ImageStorage createImageBackup(UserImage userImage, String image) {
