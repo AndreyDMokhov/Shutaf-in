dee59e16d60af97b62463f2a29dd044ef04f107c
Marina <maryna.pekh@gmail.com>
2017-08-08 13:15:34 +0300

    Marina Pekh

--- si-core/src/main/java/com/shutafin/service/UserSearchService.java
+++ si-core/src/main/java/com/shutafin/service/UserSearchService.java
@@ -1,7 +1,5 @@
 package com.shutafin.service;
 
-import com.shutafin.model.entities.User;
-import com.shutafin.model.entities.UserSearch;
 import com.shutafin.model.web.user.UserSearchWeb;
 
 import java.util.List;
