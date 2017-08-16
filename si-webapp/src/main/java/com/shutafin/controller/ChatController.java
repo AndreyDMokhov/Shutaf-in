e94c9afd96208ebcb14233ea5c534ec6b466190c
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-08-16 13:21:22 +0300

    Anatoliy Tsybikov 1 ChatController -> add @AuthenticatedUser User user

--- si-webapp/src/main/java/com/shutafin/controller/ChatController.java
+++ si-webapp/src/main/java/com/shutafin/controller/ChatController.java
@@ -6,6 +6,7 @@ import com.shutafin.model.entities.ChatMessage;
 import com.shutafin.model.entities.User;
 import com.shutafin.model.web.chat.ChatMessageInputWeb;
 import com.shutafin.model.web.chat.ChatMessageOutputWeb;
+import com.shutafin.processors.annotations.authentication.AuthenticatedUser;
 import com.shutafin.service.ChatManagementService;
 import com.shutafin.service.SessionManagementService;
 import org.apache.commons.lang3.StringUtils;
@@ -30,8 +31,7 @@ public class ChatController {
     private ChatManagementService chatManagementService;
 
     @RequestMapping(value = "/new/{chat_title}", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public void addChat(@PathVariable("chat_title") String chatTitle, HttpServletRequest request, HttpServletResponse response){
-        User user = getUserFromSession(request);
+    public void addChat(@PathVariable("chat_title") String chatTitle, @AuthenticatedUser User user, HttpServletResponse response){
         Chat chat = chatManagementService.getNewChat(chatTitle);
         chatManagementService.addChatUserToChat(user, chat);
         response.setHeader("chat_id", String.valueOf(chat.getId()));
@@ -40,21 +40,20 @@ public class ChatController {
 
     @RequestMapping(value = "/{chat_id}/add/user/{user_id}", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void addChatUser(@PathVariable("chat_id") Long chatId, @PathVariable("user_id") Long userId,
-                            HttpServletRequest request){
-        Chat chat = getChatBySessionId(request, chatId);
+                            @AuthenticatedUser User user){
+        Chat chat = getChatByUser(user, chatId);
         chatManagementService.addChatUserToChat(userId, chat);
     }
 
     @RequestMapping(value = "/{chat_id}/remove/user/{user_id}", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
     public void removeChatUser(@PathVariable("chat_id") Long chatId, @PathVariable("user_id") Long userId,
-                            HttpServletRequest request){
-        Chat chat = getChatBySessionId(request, chatId);
+                               @AuthenticatedUser User user){
+        Chat chat = getChatByUser(user, chatId);
         chatManagementService.removeChatUserFromChat(userId, chat);
     }
 
     @RequestMapping(value = "/get/chats", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public Map<Long, String> getChats(HttpServletRequest request, HttpServletResponse response){
-        User user = getUserFromSession(request);
+    public Map<Long, String> getChats(@AuthenticatedUser User user, HttpServletResponse response){
         response.setHeader("user_id", String.valueOf(user.getId()));
         return chatManagementService.getMapChats(user);
     }
@@ -67,14 +66,13 @@ public class ChatController {
     }
 
     @RequestMapping(value = "/{chat_id}/get/users", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public Map<Long, String> getUsers(@PathVariable("chat_id") Long chatId, HttpServletRequest request){
-        Chat chat = getChatBySessionId(request, chatId);
+    public Map<Long, String> getUsers(@PathVariable("chat_id") Long chatId, @AuthenticatedUser User user){
+        Chat chat = getChatByUser(user, chatId);
         return chatManagementService.getMapUsers(chat);
     }
 
     @RequestMapping(value = "/{chat_id}/get/messages", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public List<ChatMessageOutputWeb> getMessages(@PathVariable("chat_id") Long chatId, HttpServletRequest request){
-        User user = getUserFromSession(request);
+    public List<ChatMessageOutputWeb> getMessages(@PathVariable("chat_id") Long chatId, @AuthenticatedUser User user){
         Chat chat = getChatByUser(user, chatId);
         List<ChatMessage> chatMessages = chatManagementService.getListMessages(chat, user);
         return createListChatMessageOutputWeb(chatMessages);
@@ -98,29 +96,12 @@ public class ChatController {
         return chatMessageOutputWeb;
     }
 
-    private Chat getChatBySessionId(HttpServletRequest request, Long chatId) {
-        User user = getUserFromSession(request);
-        return getChatByUser(user, chatId);
-    }
-
     private Chat getChatByUser(User user, Long chatId) {
         Chat chat = chatManagementService.findAuthorizedChat(chatId, user);
         if (chat == null){
             throw new AuthenticationException();
         }
         return chat;
-    }
-
-    private User getUserFromSession(HttpServletRequest request) {
-        String sessionId = request.getHeader("session_id");
-        if (StringUtils.isBlank(sessionId)) {
-            throw new AuthenticationException();
-        }
-        User user = sessionManagementService.findUserWithValidSession(sessionId);
-        if (user == null) {
-            throw new AuthenticationException();
-        }
-        return user;
     }
 
 }
