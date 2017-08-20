c5675b8bbe27fc7ee6bc506cf85912a416e1b487
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-08-20 21:33:38 +0300

    Anatoliy Tsybikov

--- si-webapp/src/main/java/com/shutafin/processors/TraceLogBeanPostProcessor.java
+++ si-webapp/src/main/java/com/shutafin/processors/TraceLogBeanPostProcessor.java
@@ -9,12 +9,10 @@ import org.springframework.cglib.proxy.InvocationHandler;
 import org.springframework.cglib.proxy.Proxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.util.ClassUtils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,11 +24,8 @@ public class TraceLogBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         Class beanClass = bean.getClass();
-        if (beanClass.getName().contains("SMTPContextConfiguration")) {
-            System.out.println();
-        }
-        if (beanClass.getName().contains("com.shutafin") && log.isTraceEnabled() && AnnotationUtils.findAnnotation(beanClass, Configuration.class) == null) {
-            System.out.println();
+        if (beanClass.getName().contains("com.shutafin") && log.isTraceEnabled() &&
+                AnnotationUtils.findAnnotation(beanClass, Configuration.class) == null) {
             requiredProxyBeans.put(beanName, beanClass);
         }
         return bean;
@@ -44,48 +39,62 @@ public class TraceLogBeanPostProcessor implements BeanPostProcessor {
         }
         Class[] interfaces = beanClass.getInterfaces();
 
-        System.out.println("-"+beanName + " interfaces="+beanClass.getInterfaces().length);
-
         if (interfaces != null && interfaces.length > 0) {
 
             ClassLoader beanClassClassLoader = beanClass.getClassLoader();
-            Class<?>[] allInterfacesForBeanClass = ClassUtils.getAllInterfacesForClass(beanClass);
-            System.out.println("+" + beanName + " interfaces: " + Arrays.toString(interfaces) + " allInterfacesForBeanClass: " + Arrays.toString(allInterfacesForBeanClass));
 
             return Proxy.newProxyInstance(beanClassClassLoader, beanClass.getInterfaces(), (proxy, method, args) -> {
-                    System.out.println("+++++++++++++++");
-                    System.out.println("Method name: " + method.getName());
-                    System.out.println("Class name: " + beanClass.getName());
-                    System.out.println("+++++++++++++++");
-                    return executeMethod(method, bean, args);
+
+                Gson gson = new Gson();
+                String methodName = method.getName();
+
+                System.out.println("Class name: " + beanClass.getName());
+                System.out.println("Method name: " + methodName);
+
+                Parameter[] parameters = method.getParameters();
+                for (int i = 0; i < parameters.length; i++) {
+                    System.out.println(methodName + " parameter name: " + parameters[i].getName());
+                    System.out.println(methodName + " parameter type: " + parameters[i].getType().getTypeName());
+                    System.out.println(methodName + " parameter value: " + gson.toJson(args[i]));
+                }
+
+                Object methodResult = executeMethod(method, bean, args);
+                if (!method.getReturnType().getName().equals("void")){
+                    System.out.println("Method " + methodName + " result: " + gson.toJson(methodResult, method.getReturnType()));
+                }else {
+                    System.out.println("Method " + methodName + " result: "+ method.getReturnType().getName());
+                }
+
+                return methodResult;
             });
         }else{
             return Enhancer.create(beanClass, new InvocationHandler() {
                 @Override
-                public Object invoke(Object o, Method method, Object[] args) throws Throwable {
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
                     Gson gson = new Gson();
+                    String methodName = method.getName();
 
                     System.out.println("-------------");
-                    System.out.println("Method name: " + method.getName());
                     System.out.println("Class name: " + beanClass.getName());
+                    System.out.println("Method name: " + methodName);
 
                     Parameter[] parameters = method.getParameters();
                     for (int i = 0; i < parameters.length; i++) {
-                        System.out.println("Parameter name: " + parameters[i].getName());
-                        System.out.println("Parameter type: " + parameters[i].getType().getTypeName());
+                        System.out.println(methodName + " parameter name: " + parameters[i].getName());
+                        System.out.println(methodName + " parameter type: " + parameters[i].getType().getTypeName());
                         if (!parameters[i].getType().getTypeName().equals("javax.servlet.http.HttpServletResponse")){
-                            System.out.println("Parameter value: " + gson.toJson(args[i]));
+                            System.out.println(methodName + " parameter value: " + gson.toJson(args[i]));
                         }else{
-                            System.out.println("Parameter value: " + args[i]);
+                            System.out.println(methodName + " pParameter value: " + args[i]);
                         }
                     }
 
                     Object methodResult = executeMethod(method, bean, args);
                     if (!method.getReturnType().getName().equals("void")){
-                        System.out.println("Method " + method.getName() + " result: " + gson.toJson(methodResult, method.getReturnType()));
+                        System.out.println("Method " + methodName + " result: " + gson.toJson(methodResult, method.getReturnType()));
                     }else {
-                        System.out.println("Method " + method.getName() + " result: "+ method.getReturnType().getName());
+                        System.out.println("Method " + methodName + " result: "+ method.getReturnType().getName());
                     }
 
                     System.out.println("-------------");
@@ -93,7 +102,6 @@ public class TraceLogBeanPostProcessor implements BeanPostProcessor {
                 }
             });
         }
-//        return bean;
     }
 
     private Object executeMethod(Method method, Object bean, Object ... args) throws Throwable {
