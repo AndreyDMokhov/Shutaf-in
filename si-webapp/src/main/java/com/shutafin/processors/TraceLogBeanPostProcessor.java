36c476127717ad4e15548f1fa80712e55ffdc62d
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-08-20 17:43:45 +0300

    Anatoliy Tsybikov

--- si-webapp/src/main/java/com/shutafin/processors/TraceLogBeanPostProcessor.java
+++ si-webapp/src/main/java/com/shutafin/processors/TraceLogBeanPostProcessor.java
@@ -1,49 +1,72 @@
 package com.shutafin.processors;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.InvocationHandler;
+import org.springframework.cglib.proxy.Proxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.ClassUtils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 public class TraceLogBeanPostProcessor implements BeanPostProcessor {
 
-    private Map<String, Object> requiredProxyBeans = new HashMap<>();
+    private Map<String, Class> requiredProxyBeans = new HashMap<>();
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         Class beanClass = bean.getClass();
-        if (beanClass.getName().contains("com.shutafin") && log.isTraceEnabled()) {
-//            System.out.println(beanClass.getName());
-//            System.out.println("-------------");
-            requiredProxyBeans.put(beanName, bean);
+        if (beanClass.getName().contains("SMTPContextConfiguration")) {
+            System.out.println();
+        }
+        if (beanClass.getName().contains("com.shutafin") && log.isTraceEnabled() && AnnotationUtils.findAnnotation(beanClass, Configuration.class) == null) {
+            System.out.println();
+            requiredProxyBeans.put(beanName, beanClass);
         }
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (requiredProxyBeans.get(beanName) == null) {
+        Class beanClass = requiredProxyBeans.get(beanName);
+        if (beanClass == null) {
             return bean;
         }
-        Class beanClass = bean.getClass();
+        Class[] interfaces = beanClass.getInterfaces();
 
-        if (beanClass.getInterfaces().length == 0) {
+        System.out.println("-"+beanName + " interfaces="+beanClass.getInterfaces().length);
+
+        if (interfaces != null && interfaces.length > 0) {
+
+            ClassLoader beanClassClassLoader = beanClass.getClassLoader();
+            Class<?>[] allInterfacesForBeanClass = ClassUtils.getAllInterfacesForClass(beanClass);
+            System.out.println("+" + beanName + " interfaces: " + Arrays.toString(interfaces) + " allInterfacesForBeanClass: " + Arrays.toString(allInterfacesForBeanClass));
+
+            return Proxy.newProxyInstance(beanClassClassLoader, beanClass.getInterfaces(), (proxy, method, args) -> {
+                    System.out.println("+++++++++++++++");
+                    System.out.println("Method name: " + method.getName());
+                    System.out.println("Class name: " + beanClass.getName());
+                    System.out.println("+++++++++++++++");
+                    return executeMethod(method, bean, args);
+            });
+        }else{
             return Enhancer.create(beanClass, new InvocationHandler() {
                 @Override
                 public Object invoke(Object o, Method method, Object[] args) throws Throwable {
-                    System.out.println("-------------");
 
+                    Gson gson = new Gson();
+
+                    System.out.println("-------------");
                     System.out.println("Method name: " + method.getName());
                     System.out.println("Class name: " + beanClass.getName());
 
@@ -51,52 +74,26 @@ public class TraceLogBeanPostProcessor implements BeanPostProcessor {
                     for (int i = 0; i < parameters.length; i++) {
                         System.out.println("Parameter name: " + parameters[i].getName());
                         System.out.println("Parameter type: " + parameters[i].getType().getTypeName());
-
-
-                        ObjectMapper mapper = new ObjectMapper();
-                        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-
-                        try {
-                            System.out.println("Parameter value: " + mapper.writeValueAsString(args[i]));
-                        } catch (JsonProcessingException e) {
-                            e.printStackTrace();
+                        if (!parameters[i].getType().getTypeName().equals("javax.servlet.http.HttpServletResponse")){
+                            System.out.println("Parameter value: " + gson.toJson(args[i]));
+                        }else{
+                            System.out.println("Parameter value: " + args[i]);
                         }
-
-//                        Gson gson = new Gson();
-//                        GsonBuilder builder = new GsonBuilder();
-//                        builder.serializeNulls();
-//                        builder.enableComplexMapKeySerialization();
-//                        Gson gson = builder.create();
-//                        System.out.println("Parameter value: " + gson.toJson(args[i]));
-
-
-
-                        //                        if (parameters[i].getType().getTypeName().contains("java.lang.")){
-//                            System.out.println("Parameter value: " + args[i]);
-//                        }else{
-//                        }
-//                        if (parameters[i].getType().getName().contains("com.shutafin")){
-//                            Field[] declaredFields = args[i].getClass().getDeclaredFields();
-//                            for (Field declaredField : declaredFields) {
-//                                System.out.println(parameters[i].getType().getSimpleName() + " field name: " + declaredField.getName());
-//                                System.out.println(parameters[i].getType().getSimpleName() + " field type: " + declaredField.getType().getName());
-//                                declaredField.setAccessible(true);
-//                                Class<?> fieldClass = Class.forName(declaredField.getType().getTypeName());
-//                                Object obj = fieldClass.newInstance();
-//                                System.out.println(parameters[i].getType().getSimpleName() + " field value: " + declaredField.get(args[i]));
-//                            }
-//                        }else {
-//                            System.out.println("Parameter value: " + args[i]);
-//                        }
-
                     }
-                    System.out.println("-------------");
 
-                    return executeMethod(method, bean, args);
+                    Object methodResult = executeMethod(method, bean, args);
+                    if (!method.getReturnType().getName().equals("void")){
+                        System.out.println("Method " + method.getName() + " result: " + gson.toJson(methodResult, method.getReturnType()));
+                    }else {
+                        System.out.println("Method " + method.getName() + " result: "+ method.getReturnType().getName());
+                    }
+
+                    System.out.println("-------------");
+                    return methodResult;
                 }
             });
         }
-        return bean;
+//        return bean;
     }
 
     private Object executeMethod(Method method, Object bean, Object ... args) throws Throwable {
