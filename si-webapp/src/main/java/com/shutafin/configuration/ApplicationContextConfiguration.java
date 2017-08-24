a7127ab902a904b7c5a6cb1b1463ddfa0ef41729
Anatoliy Tsybikov <tal2x8@gmail.com>
2017-08-25 02:11:36 +0300

    Anatoliy Tsybikov

--- si-webapp/src/main/java/com/shutafin/configuration/ApplicationContextConfiguration.java
+++ si-webapp/src/main/java/com/shutafin/configuration/ApplicationContextConfiguration.java
@@ -1,11 +1,8 @@
 package com.shutafin.configuration;
 
 import com.shutafin.processors.AuthenticationAnnotationsBeanPostProcessor;
-<<<<<<<<< Temporary merge branch 1
-import com.shutafin.processors.TraceLogBeanPostProcessor;
-=========
 import com.shutafin.processors.SessionResponseAnnotationBeanPostProcessor;
->>>>>>>>> Temporary merge branch 2
+import com.shutafin.processors.TraceLogBeanPostProcessor;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -32,13 +29,13 @@ public class ApplicationContextConfiguration {
     }
 
     @Bean
-<<<<<<<<< Temporary merge branch 1
-    public TraceLogBeanPostProcessor traceLogBeanPostProcessor() {
-        return new TraceLogBeanPostProcessor();
-=========
     public SessionResponseAnnotationBeanPostProcessor sessionAnnotationsResponseBeanPostProcessor(){
         return new SessionResponseAnnotationBeanPostProcessor();
->>>>>>>>> Temporary merge branch 2
+    }
+
+    @Bean
+    public TraceLogBeanPostProcessor traceLogBeanPostProcessor() {
+        return new TraceLogBeanPostProcessor();
     }
 
 }
