869e2522e3827e39082392d91ebc9142df1ccc45
khomenker <autohelp123@gmail.com>
2017-08-31 13:24:27 +0300

    Shimon Khomenker resolved conflicts

--- si-webapp/src/client/src/partials/questions/questions.ctrl.js
+++ si-webapp/src/client/src/partials/questions/questions.ctrl.js
@@ -1,34 +1,232 @@
-app.controller('QuestionsCtrl', function ($scope, $state) {
-    $scope.answers = [];
-    $scope.back = function () {
+app.controller('QuestionsCtrl', function ($scope, $state, quizFactory) {
+    var result = [];
+    $scope.answersFromController = [];
+    $scope.questionsFromController = [];
+    $scope.back = function (data) {
+        console.log(data);
         $state.go('home')
+    };
+    $scope.coooo = function () {
+        // console.log($scope.answersFromController);
     }
+
+    $scope.isReady = false;
+
+    function getQuestion() {
+        // var deferred = $q.defer();
+        // quizFactory.get123(); ->>    LETTER. We don't pay with letters. We pay with money. Open the letter
+        //get123 -> письмо
+        //then -> открывашка
+        //money -> деньги
+        quizFactory.get123().then(function (money) {
+
+            $scope.questionsFromController = money;
+            $scope.isReady = true;
+            // return deferred.resolve($scope.questionsFromController);
+        }, function (noMoney) {
+
+        });
+        // return deferred.promise;
+    }
+
+    getQuestion();
+
+    // function test() {
+    //     getQuestion().then(function (result) {
+    //
+    //         console.log('!')
+    //         console.log($scope.questionsFromController)
+    //     })
+    // }
+    // console.log('ttt')
+    // test();
+
 });
 
-app.directive('quiz', function (quizFactory) {
+
+app.directive('quiz', function () {
     return {
+        scope: {
+            questionsToDirective: '=',
+            answerToDirective: '=',
+            backD:'&',
+            returnAnswer: '='
+        },
         restrict: 'AE',
         templateUrl: 'partials/questions/questionTemplate.html',
-
         link: function (scope, elem, attrs) {
+            var questions = scope.questionsToDirective;
+            var answer = scope.answerToDirective;
+            var back = scope.back;
+            var rrr = scope.returnAnswer;
+            // console.log(questions)
             scope.inProgress = true;
-            scope.currentAnswer;
-            scope.id = 0
-            scope.currentQuestion = quizFactory.getQuestion(scope.id);
 
+            scope.id = 0;
+            scope.currentQuestion = getQuestion1(scope.id);
+            // console.log(scope.currentQuestion.answers);
 
             scope.nextQuestion = function () {
-                var result = {"idQ": scope.currentQuestion.id, "idA": scope.currentAnswer}
-                scope.answers.push(result)
+                var result = {"idQ": scope.currentQuestion.id, "idA": scope.currentAnswer};
+                answer.push(result.idA);
                 scope.id++;
-                scope.currentQuestion = quizFactory.getQuestion(scope.id);
+                scope.currentQuestion =getQuestion1(scope.id);
                 if (!scope.currentQuestion) {
                     scope.inProgress = false;
-                    console.log(scope.answers);
-                    scope.back();
+                    console.log(answer);
+                    scope.backD({data: answer});
                 }
                 scope.currentAnswer = null;
+            };
+
+            function getQuestion1(id) {
+                if (id < questions.length) {
+                    return questions[id];
+                } else {
+                    return false;
+                }
             }
         }
     }
 });
+
+// (function(angular) {
+//     'use strict';
+//
+//         app.controller('QuestionsCtrl', ['$scope', function($scope,quizFactory) {
+//             $scope.customer = {
+//                 name: 'Naomi',
+//                 address: '1600 Amphitheatre'
+//             };
+//             //
+//             $scope.answers = [];
+//     $scope.questions=123;
+//     $scope.back = function () {
+//         $state.go('home')
+//     };
+//                 function getQuestion() {
+//         quizFactory.get123().then(
+//             function (success) {
+//                 $scope.questions = success.data.data;
+//                 console.log($scope.questions)
+//             },
+//             function (error) {
+//                 console.log("---")
+//                 console.log(error)
+//
+//             }
+//         )
+//
+//     }
+//         getQuestion();
+//             console.log($scope.questions)
+//
+//     // function getQuestion() {
+//     //     quizFactory.get123().then(
+//     //         function (success) {
+//     //             $scope.questions = success.data.data;
+//     //             console.log($scope.questions)
+//     //         },
+//     //         function (error) {
+//     //             console.log("---");
+//     //             console.log(error)
+//     //
+//     //         }
+//     //     )
+//     //
+//     // }
+//     //     getQuestion();
+//
+//
+//         }]);
+//         app.directive('myCustomer', function() {
+//             // return {
+//             //     template: 'Name: {{questions}} Address: {{customer.address}}'
+//             // };
+//
+//             // app.directive('quiz', function (quizFactory) {
+//
+//             return {
+//
+//                 restrict: 'AE',
+//                 templateUrl: 'partials/questions/questionTemplate.html',
+//                 controller: function ($scope) {
+//
+//                 },
+//                 link: function (scope, elem, attrs, $scope) {
+//                     var questions = [
+//                         {
+//                             id:5,
+//                             question: "Question1",
+//                             options: ["answer1", "answer2", "answer3", "answer4"],
+//                         },
+//                         { id:6,
+//                             question: "Question2",
+//                             options: ["answer1", "answer2"],
+//                         },
+//                         {
+//                             id:7,
+//                             question: "Question3",
+//                             options: ["answer1", "answer2", "answer3"],
+//                         },
+//                         {
+//                             id:8,
+//                             question: "Question4",
+//                             options: ["answer1", "answer2", "answer3", "answer4"],
+//                         },
+//                         {
+//                             id:9,
+//                             question: "Question5",
+//                             options: ["answer1", "answer2", "answer3", "answer4"],
+//
+//                         }
+//                     ];
+//                     scope.inProgress = true;
+//                     scope.currentAnswer;
+//                     scope.id = 0
+//                     scope.currentQuestion = getQuestion1(scope.id);
+//
+//
+//                     scope.nextQuestion = function () {
+//                         var result = {"idQ": scope.currentQuestion.id, "idA": scope.currentAnswer}
+//                         scope.answers.push(result)
+//                         scope.id++;
+//                         scope.currentQuestion = getQuestion1(scope.id);
+//                         if (!scope.currentQuestion) {
+//                             scope.inProgress = false;
+//                             console.log(scope.answers);
+//                             scope.back();
+//                         }
+//                         scope.currentAnswer = null;
+//                     };
+//                     function getQuestion1(id) {
+//                         if (id < questions.length) {
+//                             return questions[id];
+//                         } else {
+//                             return false;
+//                         }
+//                     }
+//                 }
+//             }
+//
+//
+//
+//
+//         });
+// })(window.angular);
+
+//
+//     $scope.questionsFromController = 'govno';
+//     console.log(  $scope.questionsFromController)
+//     function getQuestion() {
+//         quizFactory.get123().then(
+//             function (success) {
+//                 $scope.questionsFromController = success.data.data;
+//                 console.log(  $scope.questionsFromController)
+//             },
+//             function (error) {
+//                 console.log(error)
+//             }
+//         )}
+//     getQuestion();
+// console.log(  $scope.questionsFromController)
--- si-webapp/src/client/src/partials/questions/questions.model.js
+++ si-webapp/src/client/src/partials/questions/questions.model.js
@@ -1,39 +1,40 @@
-app.factory('quizFactory', function () {
-    var questions = [
-        {
-            id:5,
-            question: "Question1",
-            options: ["answer1", "answer2", "answer3", "answer4"],
-        },
-        { id:6,
-            question: "Question2",
-            options: ["answer1", "answer2"],
-        },
-        {
-            id:7,
-            question: "Question3",
-            options: ["answer1", "answer2", "answer3"],
-            },
-            {
-                id:8,
-                question: "Question4",
-                options: ["answer1", "answer2", "answer3", "answer4"],
-            },
-            {
-                id:9,
-                question: "Question5",
-                options: ["answer1", "answer2", "answer3", "answer4"],
+app.factory('quizFactory', function (Restangular, $q, $sessionStorage) {
+    var rest = Restangular.withConfig(function (RestangularProvider) {
+        RestangularProvider.setFullResponse(true);
+        RestangularProvider.setBaseUrl('/api/user/match');
+        RestangularProvider.setDefaultHeaders({'session_id': $sessionStorage.sessionId});
+    });
+    var data = {};
 
-        }
-    ];
+    //10 000 SHEKEL
+    function get123() {
+        // return rest.one('/template').customGET();
+
+        //Сделка. Нужно подождать, я вернусь с ответом
+        //$q.defer()
+        var deferred = $q.defer();
+        data = rest.one("/template").withHttpConfig({timeout: 10000});
+
+        data.get().then(
+            function (success) {
+                data.questionAnswers = success.data.data;
+                //deferred.resolve -> конверт
+                //data.questionAnswers -> деньги
+                return deferred.resolve(data.questionAnswers); // -> конверт с деньгами внутри
+            },
+            function (error) {
+                return deferred.reject(); // нет денег
+            }
+        );
+        return deferred.promise; // -> Я обещаю, что я вернусь!!
+
+
+    }
 
     return {
-        getQuestion: function (id) {
-            if (id < questions.length) {
-                return questions[id];
-            } else {
-                return false;
-            }
-        }
+
+        get123: get123
+
+
     };
 });
