2245640c66807884bdb2d5b082c4b25ae10eff16
Rogov <creynir@gmail.com>
2017-09-04 22:17:26 +0300

    Michail Rogov Front: userSettings.ctrl.js?userSettings.html - removed ng repeat. code clean up

--- si-webapp/src/client/src/partials/userSettings/userSettings.ctrl.js
+++ si-webapp/src/client/src/partials/userSettings/userSettings.ctrl.js
@@ -10,24 +10,11 @@ app.controller("userSettingsController", function ($localStorage, $sessionStorag
     vm.cities = $localStorage.cities;
     vm.gender = $localStorage.genders;
 
-
     // if number of countries more than one, delete next line
     vm.userProfile.countryId = 1;
 
-    if (vm.userProfile.countryId !== null) {
-        vm.userProfile.countryId = vm.userProfile.countryId.toString();
-    }
-    if (vm.userProfile.genderId !== null) {
-        vm.userProfile.genderId=vm.userProfile.genderId.toString();
-    }
-    if (vm.userProfile.cityId !== null) {
-        vm.userProfile.cityId=vm.userProfile.cityId.toString();
-    }
-
-
     function submitChanges() {
         vm.dataLoading = true;
-
         userSettingsModel.saveDataPostRegistration(vm.userProfile).then(
             function (success) {
                 notify.set($filter('translate')('UserSettings.message.save.success'), {type: 'success'});
