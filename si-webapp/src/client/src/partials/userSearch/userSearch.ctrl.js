f9b40cbc8adab106a0c767031839d9e40da6e8ae
edwardKats <edward.kats@outlook.com>
2017-10-06 04:22:10 +0300

    Edward Kats 1. Added user profile image to the header 2. Fixed image displaying for user search

--- si-webapp/src/client/src/partials/userSearch/userSearch.ctrl.js
+++ si-webapp/src/client/src/partials/userSearch/userSearch.ctrl.js
@@ -32,7 +32,7 @@ app.controller("userSearchController", function ($state, $sessionStorage, notify
     }
 
     function getImage(userProfile) {
-        if (!userProfile.userImageId) {
+        if (!userProfile.userImage) {
             return '../../images/default_avatar.png';
         }
         else {
