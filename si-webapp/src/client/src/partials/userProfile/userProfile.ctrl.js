58f55aea2b6945c08ad9f4d4b9828b225fe4a26b
khomenker <autohelp123@gmail.com>
2017-10-08 19:22:46 +0300

    Shimon Khomenker 1. created template for imagePopup 2. added style for imagePopup 3. created new functions in controller for getting crop image

--- si-webapp/src/client/src/partials/userProfile/userProfile.ctrl.js
+++ si-webapp/src/client/src/partials/userProfile/userProfile.ctrl.js
@@ -8,16 +8,14 @@ app.controller('userProfileController', function ($localStorage,
                                                   notify,
                                                   $timeout,
                                                   $scope,
+                                                  ngDialog,
                                                   IMAGE_MAX_SIZE_MB) {
 
     var vm = this;
-    $scope.myImage = 'resources/yyy.jpg';
     $scope.myCroppedImage='';
-    vm.invisible = true;
     vm.userProfile = $sessionStorage.userProfile;
     vm.fileInfo = {};
-    vm.size = 2000;
-    // vm.size = IMAGE_MAX_SIZE_MB * 1000;
+    vm.size = IMAGE_MAX_SIZE_MB * 1000;
 
     vm.cities = $sessionStorage.cities;
     vm.genders = $sessionStorage.genders;
@@ -34,11 +32,11 @@ app.controller('userProfileController', function ($localStorage,
 
     $scope.onLoad = function (e, reader, file, fileList, fileOjects, fileObj) {
         $timeout(function () {
-            vm.image = 'data:image/jpeg;base64,' + vm.fileInfo.base64;
+            $scope.myImage = 'data:image/jpeg;base64,' + vm.fileInfo.base64;
             vm.deleteButton = true;
-            vm.invisible = false;
+
             if (vm.size > vm.fileInfo.filesize / 1024) {
-                saveImage();
+                $scope.showImagePopup();
             }
             else {
                 setProfileImage();
@@ -49,8 +47,8 @@ app.controller('userProfileController', function ($localStorage,
 
     };
 
-    function saveImage() {
-        userProfileModel.addOrUpdateImage({image: vm.fileInfo.base64}).then(
+    function saveImage(data) {
+        userProfileModel.addOrUpdateImage({image: data}).then(
             function (success) {
                 vm.userProfile.userImage = success.data.data.image;
                 vm.userProfile.userImageId = success.data.data.id;
@@ -61,7 +59,6 @@ app.controller('userProfileController', function ($localStorage,
             },
 
             function (error) {
-
                 if (error === undefined || error === null) {
                     notify.set($filter('translate')('Error.SYS'), {type: 'error'});
                 }
@@ -86,18 +83,28 @@ app.controller('userProfileController', function ($localStorage,
                 vm.deleteButton = true;
                 notify.set($filter('translate')('UserProfile.message.imageDeleted'), {type: 'success'});
             },
-
             function (error) {
                 notify.set($filter('translate')('Error' + '.' + error.data.error.errorTypeCode), {type: 'error'});
             });
     }
-    function doIt() {
-     vm.invisible = true;
-     vm.image = $scope.myCroppedImage;
-    }
+    $scope.cropIt =function(data) {
+        vm.image=data;
+        var arr = data.split([',']);
+        saveImage(arr[1]);
+        ngDialog.close();
+    };
+
+    $scope.showImagePopup = function() {
+        ngDialog.open({
+            templateUrl: 'partials/userProfile/imagePopup.html',
+            scope: $scope,
+            showClose: false,
+            className: 'ngdialog-theme-plain custom-width',
+            closeByDocument: true
+        });
+    };
 
     setProfileImage();
     vm.saveImage = saveImage;
     vm.deleteImage = deleteImage;
-    vm.doIt = doIt;
 });
