0d1c48f0a1fabda70144787bb611c9368bbb460a
Rogov <creynir@gmail.com>
2017-11-02 23:40:14 +0200

    Merge branch 'jpa/migration' of https://bitbucket.org/edwardKats/shutaf-in into michail/messenger
    changed all settings tabs to component system
    
    - New files:
    
    changeEmail.component.html
    changeEmail.component.js
    changePassword.component.html
    changePassword.component.js
    userSettings.component.html
    userSettings.component.js
    settings.component.html
    settings.component.js
    
    - Deleted:
    
    changeEmail.request.ctrl.js
    changePassword.ctrl.js
    userSettings.ctrl.js
    settings.ctrl.js
    settings.model.js
    
    - Changed:
    
    index.html - replaced old links with new ones
    changeEmail.html/changePassword.html/userSettings.html/settings.html - replaced html code with component reference

---
+++ si-webapp/src/client/src/partials/settings/changePassword/changePassword.component.js
@@ -0,0 +1,33 @@
+"use strict";
+app.component('changePasswordComponent', {
+    templateUrl: 'partials/settings/changePassword/changePassword.component.html',
+    bindings: {},
+    controllerAs: 'vm',
+    controller: function ($rootScope, changePasswordModel, notify, $filter, $state) {
+
+        var vm = this;
+        vm.isOpened = true;
+        vm.dataLoading = false;
+        vm.securitySettings = {};
+
+        function changePassword() {
+            vm.dataLoading = true;
+
+            changePasswordModel.changePassword(vm.securitySettings).then(
+                function (success) {
+                    vm.dataLoading = false;
+                    notify.set($filter('translate')("Settings.security.msg.success"), {type: 'success'});
+                    $state.go('settings');
+                }, function (error) {
+                    vm.dataLoading = false;
+
+                    notify.set($filter('translate')('Error' + '.' + error.data.error.errorTypeCode), {type: 'error'});
+                    if (error.data.error.errorTypeCode === 'AUT') {
+                        $state.go('logout');
+                    }
+                });
+        }
+
+        vm.changePassword = changePassword;
+    }
+});
