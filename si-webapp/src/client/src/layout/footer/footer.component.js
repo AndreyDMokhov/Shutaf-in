312fb5e092b3383a9ba651dcd04d586706163380
Andrew Mokhov <mohov@yandex.ru>
2017-11-08 08:43:51 +0200

    1. Created component  "footer.component", js and html files. 2. Added tag <footer-component> to layout component. 3. Added footer component to "index.html". 4. Added word "Help" to "lang-en.json" and "lang-ru.json"

---
+++ si-webapp/src/client/src/layout/footer/footer.component.js
@@ -0,0 +1,11 @@
+"use strict";
+app.component("footerComponent", {
+    templateUrl: 'layout/footer/footer.component.html',
+    bindings: {},
+    controllerAs: 'vm',
+    controller: function () {
+
+    }
+
+
+});
