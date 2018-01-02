bb24b1f3e113cf382fd526532ec47d7397a3aa84
Edward <edward.kats@outlook.com>
2018-01-02 03:32:32 +0200

    Edward Kats 1. Removed sql scripts 2. 100% migration to liquibase 3. Added database auto-creation 4. Fixed liquibase scripts 5. Changed databases names 6. GW: Moved all liquibase scripts to a single file 7. Added application-dev and application-prod to GW

--- si-gateway/si-gateway-core/src/main/java/com/shutafin/service/UserImageService.java
+++ si-gateway/si-gateway-core/src/main/java/com/shutafin/service/UserImageService.java
@@ -14,7 +14,6 @@ public interface UserImageService {
 
     void deleteUserImage(Long userId, Long userImageId);
 
-    //    List<UserImage> getAllUserImages(Long userId);
     String getDefaultImageBase64();
 
 }
