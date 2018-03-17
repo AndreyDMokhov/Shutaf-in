460e2c51a7b8787e9188b2674c0c3e4287248503
Edward <edward.kats@outlook.com>
2018-03-17 21:50:31 +0200

    Edward Kats 1. Added new exception for indication creation or initialization attempt of multiple deals for a user

--- si-share/src/main/java/com/shutafin/model/exception/exceptions/MultipleDealsException.java
+++ si-share/src/main/java/com/shutafin/model/exception/exceptions/MultipleDealsException.java
@@ -1,4 +1,19 @@
 package com.shutafin.model.exception.exceptions;
 
-public class MultipleDealsException {
+import com.shutafin.model.error.ErrorType;
+import com.shutafin.model.exception.AbstractAPIException;
+
+public class MultipleDealsException extends AbstractAPIException {
+
+    public MultipleDealsException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public MultipleDealsException() {
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.MULTIPLE_DEALS_ERROR;
+    }
 }
