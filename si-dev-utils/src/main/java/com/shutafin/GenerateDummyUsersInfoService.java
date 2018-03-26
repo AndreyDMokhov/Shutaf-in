395a7a7e1e5aaf18467aa69fc1aa23107a10209b
Anatoliy Tsybikov <tal2x8@gmail.com>
2018-03-26 18:00:35 +0300

    Anatoliy Tsybikov 1 GenerateDummyUsersInfoService -> add change email 2 GenerateDummyUsersService -> add methods getFirstName and getLastName

--- si-dev-utils/src/main/java/com/shutafin/GenerateDummyUsersInfoService.java
+++ si-dev-utils/src/main/java/com/shutafin/GenerateDummyUsersInfoService.java
@@ -2,6 +2,7 @@ package com.shutafin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shutafin.image.Image;
+import com.shutafin.model.web.account.AccountEmailChangeRequest;
 import com.shutafin.model.web.account.AccountUserImageWeb;
 import com.shutafin.model.web.account.AccountUserInfoRequest;
 import lombok.SneakyThrows;
@@ -14,14 +15,15 @@ import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 import static com.shutafin.GenerateDummyUsersService.getEmail;
-import static com.shutafin.GenerateDummyUsersService.getName;
+import static com.shutafin.GenerateDummyUsersService.getFirstName;
+import static com.shutafin.GenerateDummyUsersService.getLastName;
 
 @Service
 public class GenerateDummyUsersInfoService {
 
     private static final String URL_UPDATE_USER_INFO = "http://localhost:8000/users/info";
+    private static final String URL_UPDATE_EMAIL = "http://localhost:8000/users/{userId}/change-email";
     private static final String URL_UPDATE_USER_IMAGE = "http://localhost:8000/users/profile-image";
-    private static final String URL_UPDATE_ACCOUNT_STATUS = "http://localhost:8000/users/account-status";
     private static final Integer CITY_ID = 1;
     private static final Integer GENDER_ID = 1;
 
@@ -37,18 +39,21 @@ public class GenerateDummyUsersInfoService {
     public CompletableFuture<String> generateUsersInfo(int userIdFrom, int userIdTo) {
         for (int i = userIdFrom; i < userIdTo; i++) {
             restTemplateService.sendRequest(getAccountUserInfoRequest(i), URL_UPDATE_USER_INFO + "?userId=" + i, HttpMethod.PUT);
+            restTemplateService.sendRequest(getAccountEmailChangeRequest(i), URL_UPDATE_EMAIL.replace("{userId}", Integer.toString(i)), HttpMethod.PUT);
             restTemplateService.sendRequest(getAccountUserImageWeb(), URL_UPDATE_USER_IMAGE + "?userId=" + i, HttpMethod.POST);
-            restTemplateService.sendRequest(null, URL_UPDATE_ACCOUNT_STATUS + "?userId=" + i, HttpMethod.GET);
-
         }
         return CompletableFuture.completedFuture(null);
     }
 
-    private Object getAccountUserInfoRequest(int num) {
+    private Object getAccountEmailChangeRequest(int n) {
+        return new AccountEmailChangeRequest(getEmail(n));
+    }
+
+    private Object getAccountUserInfoRequest(int n) {
         return AccountUserInfoRequest.builder()
-                .firstName(getName())
-                .lastName(getName())
-                .email(getEmail(num))
+                .firstName(getFirstName(n))
+                .lastName(getLastName(n))
+                .email(getEmail(n))
                 .cityId(CITY_ID)
                 .genderId(GENDER_ID)
                 .dateOfBirth(new Date())
--- si-dev-utils/src/main/java/com/shutafin/GenerateDummyUsersService.java
+++ si-dev-utils/src/main/java/com/shutafin/GenerateDummyUsersService.java
@@ -5,20 +5,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 @Service
 public class GenerateDummyUsersService {
 
     private static final String URL_REGISTRATION = "http://localhost:8080/api/users/registration/request";
-    private static final int COUNT_CHARACTER_NAME = 3;
     private static final String PASSWORD = "11111111";
     private static final Integer USER_LANGUAGE_ID = 1;
     private static final String EMAIL_PREFIX = "tel";
-    private static final String EMAIL_SUFFIX = "@ran";
-
-    private static Random random = new Random();
+    private static final String EMAIL_SUFFIX = "ran";
 
     private RestTemplateService restTemplateService;
 
@@ -36,28 +32,24 @@ public class GenerateDummyUsersService {
 
     private AccountRegistrationRequest getRegistrationRequestWeb(int n) {
         return AccountRegistrationRequest.builder()
-                .firstName(getName())
-                .lastName(getName())
-                .email(getEmail(n))
+                .firstName(getFirstName(n))
+                .lastName(getLastName(n))
+                .email("_" + getEmail(n))
                 .password(PASSWORD)
                 .userLanguageId(USER_LANGUAGE_ID)
                 .build();
     }
 
-    public static String getName() {
-        StringBuilder name = new StringBuilder(COUNT_CHARACTER_NAME);
-        for (int i = 0; i < COUNT_CHARACTER_NAME; i++) {
-            name.append(getChar());
-        }
-        return name.toString();
+    public static String getFirstName(int n){
+        return EMAIL_PREFIX + n;
     }
 
-    private static char getChar() {
-        return (char) (random.nextInt('z' - 'a' + 1) + 'a');
+    public static String getLastName(int n){
+        return EMAIL_SUFFIX + n;
     }
 
     public static String getEmail(int number) {
-        return EMAIL_PREFIX + Integer.toString(number) + EMAIL_SUFFIX;
+        return EMAIL_PREFIX + number + "@" + EMAIL_SUFFIX;
     }
 
 }
