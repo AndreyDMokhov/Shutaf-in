f3b70c9f34ca9885ddb73ef1e2f3d34bb98b481b
Anatoliy Tsybikov <tal2x8@gmail.com>
2018-04-09 18:19:27 +0300

    Anatoliy Tsybikov 1 userSearch.model.js -> function saveFilters add page,results

--- si-gateway/si-gateway-webapp/src/client/src/partials/userSearch/userSearch.model.js
+++ si-gateway/si-gateway-webapp/src/client/src/partials/userSearch/userSearch.model.js
@@ -15,12 +15,14 @@ app.factory('userSearchModel', function ($sessionStorage, Restangular) {
         return rest.one('/api/users/search?page='+showPage+'&results='+pageSize).customGET();
     }
 
-    function saveFilters(params,fullName) {
+    function saveFilters(params,fullName,page,results) {
         rest.setDefaultHeaders({'session_id': $sessionStorage.sessionId});
+        var showPage = (page=='' || page == undefined) ? 0 : page ;
+        var pageSize = (results=='' || results == undefined) ? 10 : results ;
         if (fullName) {
-            return rest.one('/api/users/search/save/filters?name=' + fullName).customPOST(params);
+            return rest.one('/api/users/search/save/filters?page='+showPage+'&results='+pageSize+'&name=' + fullName).customPOST(params);
         }
-        return rest.one('/api/users/search/save/filters').customPOST(params);
+        return rest.one('/api/users/search/save/filters?page='+showPage+'&results='+pageSize).customPOST(params);
     }
 
     function getCompressedUserImageById(userId) {
