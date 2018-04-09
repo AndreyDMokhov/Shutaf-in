55fd394dad620623c110290e0083a5ab1aeb43d0
Edward <edward.kats@outlook.com>
2018-04-10 01:23:34 +0300

    Edward Kats 1. Minor code change

--- si-gateway/si-gateway-webapp/src/client/src/partials/userSearch/userSearch.model.js
+++ si-gateway/si-gateway-webapp/src/client/src/partials/userSearch/userSearch.model.js
@@ -15,14 +15,12 @@ app.factory('userSearchModel', function ($sessionStorage, Restangular) {
         return rest.one('/api/users/search?page='+showPage+'&results='+pageSize).customGET();
     }
 
-    function saveFilters(params,fullName,page,results) {
+    function saveFilters(params,fullName) {
         rest.setDefaultHeaders({'session_id': $sessionStorage.sessionId});
-        var showPage = (page=='' || page == undefined) ? 0 : page ;
-        var pageSize = (results=='' || results == undefined) ? 10 : results ;
         if (fullName) {
-            return rest.one('/api/users/search/save/filters?page='+showPage+'&results='+pageSize+'&name=' + fullName).customPOST(params);
+            return rest.one('/api/users/search/save/filters?page=0&results=10&name=' + fullName).customPOST(params);
         }
-        return rest.one('/api/users/search/save/filters?page='+showPage+'&results='+pageSize).customPOST(params);
+        return rest.one('/api/users/search/save/filters?page=0&results=10').customPOST(params);
     }
 
     function getCompressedUserImageById(userId) {
