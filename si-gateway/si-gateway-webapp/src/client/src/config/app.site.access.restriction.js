20167509e5a989bc15126b48462c808ea813e662
Edward <edward.kats@outlook.com>
2018-04-14 01:47:44 +0300

    Edward Kats 1. Fixed unauthenticated navigation error

--- si-gateway/si-gateway-webapp/src/client/src/config/app.site.access.restriction.js
+++ si-gateway/si-gateway-webapp/src/client/src/config/app.site.access.restriction.js
@@ -3,12 +3,54 @@
  */
 
 //https://ui-router.github.io/guide/ng1/migrate-to-1_0#state-change-events
-app.run(function($rootScope, $sessionStorage, $state, notify, $filter) {
-    $rootScope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams) {
+app.run(function ($rootScope,
+                  $state,
+                  notify,
+                  $filter,
+                  sessionService,
+                  $sessionStorage) {
+
+    var allowedUnauthenticated = [
+        'home',
+        'login',
+        'logout',
+        'registration',
+        'about',
+        'registrationConfirmation',
+        'userProfile',
+        'resetPasswordRequest',
+        'resetPasswordNewPassword',
+        'dealInitializeConfirmation',
+        'dealAddUserConfirmation',
+        'dealRemoveUserConfirmation'
+    ];
+
+    function isAllowedToNavigate(toState) {
+        if (sessionService.isAuthenticated()) {
+            return true;
+        }
+
+        for (var i = 0; i < allowedUnauthenticated.length; i++) {
+            if (allowedUnauthenticated[i] === toState.name) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    $rootScope.$on('$stateChangeStart', function (evt, toState, toParams, fromState, fromParams) {
+
+        if (!isAllowedToNavigate(toState)) {
+            notify.set($filter('translate')('Error.AUT'), {type: 'error'});
+            evt.preventDefault();
+            $state.go('home');
+            return;
+        }
 
         //If user tries to access page manually by writing URL in the address bar
-        if ($sessionStorage.accountStatus !== undefined && toParams.accessibleToAccountStatus > $sessionStorage.accountStatus){
-            switch ($sessionStorage.accountStatus){
+        if ($sessionStorage.accountStatus !== undefined && toParams.accessibleToAccountStatus > $sessionStorage.accountStatus) {
+            switch ($sessionStorage.accountStatus) {
                 case 2 :
                     evt.preventDefault();
                     notify.set($filter('translate')('SiteAccess.info.settings'), {type: 'info'});
